f798bbeaf672de7d8692f3df95f902779eafede8
saxenaishaan07-debug <saxenaishaan07@gmail.com>
2026-03-12 20:49:45 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-const elements = document.querySelectorAll(".review, .grid img, .gallery-preview img");
-
-function reveal(){
-  const windowHeight = window.innerHeight;
-  elements.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if(top < windowHeight - 100){
-      el.classList.add("show");
-    }
+window.addEventListener("load", () => {
+  document.querySelectorAll(".review, .service, .grid img").forEach(el => {
+    el.classList.add("show");
   });
-}
-
-window.addEventListener("scroll", reveal);
-window.addEventListener("load", reveal);
+});
