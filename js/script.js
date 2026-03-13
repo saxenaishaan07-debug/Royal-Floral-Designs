5256f2c97ab8bd5f4b65e30b8fb501c86110cc9c
saxenaishaan07-debug <saxenaishaan07@gmail.com>
2026-03-12 20:20:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Royal Floral Designs website loaded");
+const elements = document.querySelectorAll(".review, .grid img");
+
+function reveal(){
+
+const windowHeight = window.innerHeight;
+
+elements.forEach(el => {
+
+const elementTop = el.getBoundingClientRect().top;
+
+if(elementTop < windowHeight - 100){
+
+el.classList.add("show");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll", reveal);
